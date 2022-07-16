35e39e1267218557be0a2c4b436ef001377e9014
kaanperktas <98807449+kaanperktas@users.noreply.github.com>
2022-07-16 12:48:07 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemory());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description + "'ın modeli" + " " + car.ModelYear);
+                
+            }
+        }
+    }
+    }
+
